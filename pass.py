f33d4862f67dfc964dfbd856bdeff1c78d1bc2e4
ish3x <151656178+ish3x@users.noreply.github.com>
2024-06-28 08:53:22 -0400

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,58 @@
+# python 3
+#LosU$7I`zmjb0{/.      - bash MD%
+#_ZpqDgC*S_[cx4ZLosU$7I`zmjb0{/.        - bash MD%
+#_Zp33qDgCgC*S*S_[cx4Z21LosU$7I`zmjb0{/.       - bash MD%
+#gC*SLosU$7I`zmjb0{/.
+print("____________________________________________")
+
+def menu():
+    print("________________________________________")
+    print("[1] gmail") # for ex ==>all my gmails(ex==> :ali4233ir)(dont use(.,/,02,))
+    print("[2] fi website")# ex ==> gidikala,fradars,and...
+    print("[3] website")# kaliboy,and other...
+    print("[4] account")#insta,fackbook,snap,and other...if not user found typeing the company name *(facebook,instagram use id,)
+    print("________________________________________________")
+menu()
+print()
+
+option = int (input("Enter your option ==> "))
+
+if option == 1:
+
+    np = ("ynu7aerj6wpebmv8myf160w5ltb2js")
+    print("________________________________________________")
+    a = input("where this password used ? ")
+    password = (np + a)
+    print("________________________________________________")
+    print("your password is ==> " , password)
+    print("________________________________________________")
+
+elif option == 2:
+
+    np = ("6zsdf5qb8q")
+    print("________________________________________________")
+    a = input("where this password used ? ")
+    password = (np + a)
+    print("________________________________________________")
+    print("your password is ==> " , password)
+    print("________________________________________________")
+
+elif option == 3:
+    
+    np = ("az6k7ce2")
+    print("________________________________________________")
+    a = input("where this password used ? ")
+    password = (np + a)
+    print("________________________________________________")
+    print("your password is ==> " , password)
+    print("________________________________________________")
+
+elif option == 4:
+
+    np = ("pkmqct4f16pa8hgbv1ao4zsxw0ckxc")
+    print("________________________________________________")
+    a = input("where this password used ? ")
+    password = (np + a)
+    print("________________________________________________")
+    print("your password is ==> " , password)
+    print("________________________________________________")
